fdc345beba0e815d00e6a4768388b9085b908fd8
IcylFd <772167928@qq.com>
2020-02-14 23:33:33 +0800

    slice扩展

--- slice.go
+++ slice.go
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-01-19 15:36:18
  * @LastEditors  : lifangdi
- * @LastEditTime : 2020-01-19 15:49:33
+ * @LastEditTime : 2020-02-14 23:33:09
  */
 package main
 
@@ -41,4 +41,16 @@ func main() {
 	fmt.Println(s2)
 	s2 = s2[2:]
 	fmt.Println(s2)
+
+	// extending slice
+	arr[0], arr[2] = 0, 2
+	fmt.Println("arr=", s1)
+	s1 = arr[2:6]
+	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d\n", s1, len(s1), cap(s1))
+	s2 = s1[3:5]
+	fmt.Printf("s2=%v, len(s2)=%d, cap(s2)=%d\n", s2, len(s2), cap(s2))
+	// ptr 指向开头
+	// len
+	// cap 容量 从ptr开始到整个arr，不超过cap都可以向后扩展，不可以向前扩展
+	// s[i]不可以超过len(s)，但可以向后扩展到cap(s)
 }
